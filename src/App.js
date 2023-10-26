3c5c031098d5a68bf57a3f8a7dec56d40227e9c2
Javier Grau <javiergrau.freelance@gmail.com>
2023-10-25 21:47:42 -0300

    Add component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Greetings from './components/Greetings';
+
+function App() {
+  return (
+    <div className="App">
+      <Greetings />
+    </div>
+  );
+}
+
+export default App;
