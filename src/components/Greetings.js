1a7bab6b7c8fbbf4a03f07a36b108c89c05f199b
Javier Grau <javiergrau.freelance@gmail.com>
2023-10-25 22:02:42 -0300

    Install axios for fetching API

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,7 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function Greetings() {
+  const [greeting, setGreeting] = useState('');
+  useEffect(() => {
+    axios.get('http://127.0.0.1:3000/hello/index')
+    .then((response) => setGreeting(response.data.content))}, []);
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>{greeting}</h1>
     </div>
   );
 }
